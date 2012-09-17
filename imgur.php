ba9d1cf9b1ab7f19dd3d872ba362f785619e5405
Dionysis Zindros <dionyziz@gmail.com>
2012-09-17 18:12:40 -0400

    Add some error messages to imgur for image conversion and handle some additional cases for images in the HTML markup

--- imgur.php
+++ imgur.php
@@ -8,6 +8,7 @@
         while ( $block = fread( $handle, BLOCK_SIZE ) ) {
             $size += strlen( $block );
             if ( $size > MAX_SIZE ) {
+                echo "Maximum file size exceeded.\n";
                 break;
             }
             $buffer .= $block;
@@ -17,20 +18,38 @@
 
     if ( isset( $_GET[ 'url' ] ) ) {
         if ( !preg_match( '#^http\\://imgur\\.com/.*#', $_GET[ 'url' ] ) ) {
+            echo "The image page is not on imgur.\n";
             return;
         }
 
         $src = safeRead( $_GET[ 'url' ] );
-        $start = strlen( 'src="' ) + strpos( $src, 'src=', strpos( $src, 'image-container' ) );
+        $imageContainerPos = strpos( $src, 'image-container' );
+        if ( $imageContainerPos === false ) {
+            $imageContainerPos = strpos( $src, 'id="content"' );
+            if ( $imageContainerPos === false ) {
+                echo "No 'image-container' or 'content' div found.\n";
+                return;
+            }
+        }
+        $srcPos = strpos( $src, 'src=', $imageContainerPos );
+        if ( $srcPos === false ) {
+            echo "No image 'src' attribute found.\n";
+            return;
+        }
+        $start = strlen( 'src="' ) + $srcPos;
         $end = strpos( $src, '"', $start );
 
         $imgURL = substr( $src, $start, $end - $start );
         $type = strtolower( substr( $imgURL, -3 ) );
 
         if ( !preg_match( '#^http\\://.*(jpg|png)$#', $imgURL ) ) {
+            echo "The image source '" . htmlspecialchars( $imgURL ) . "' is not jpg or png.\n";
             return;
         }
         header( 'Content-Type: image/' . $type );
         echo safeRead( $imgURL );
+    }
+    else {
+        echo "The URL parameter is not set.\n";
     }
 ?>
